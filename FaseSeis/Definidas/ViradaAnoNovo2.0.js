993a2a454c559b3c2bb5fc04d7834aef8b5a874a
Nathan Guimarães <39135122+nathan-guimaraes@users.noreply.github.com>
2021-08-26 10:14:32 -0300

    Update

--- FaseSeis/Definidas/ViradaAnoNovo2.0.js
+++ FaseSeis/Definidas/ViradaAnoNovo2.0.js
@@ -1,9 +1,10 @@
 // Na “virada de ano novo”, um mostrador gigante no centro da cidade vai fazer a contagem junto com a galera. A regressiva de 10 segundos termina com o “Feliz ano novo!”.
 
-
-for (let index = -10; index < 0; index++) {
+var cont = 10;
+for (let index = 0; index <= 10; index++) {
     
-    console.log(`Contagem regressiva: ${Math.abs(index)}...`);    
+    //console.log(`Contagem regressiva: ${Math.abs(index)}...`);    
+    console.log(`Contagem regressiva: ${cont-index}...`);    
 }
 
 console.log("Feliz Ano Novo!!!");
