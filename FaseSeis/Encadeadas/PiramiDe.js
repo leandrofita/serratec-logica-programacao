b4b429e1b7a78791d19e0bd02f89dc1367ff028b
Nathan Guimarães <39135122+nathan-guimaraes@users.noreply.github.com>
2021-08-26 19:11:19 -0300

    Update

--- FaseSeis/Encadeadas/PiramiDe.js
+++ FaseSeis/Encadeadas/PiramiDe.js
@@ -5,38 +5,38 @@
 // DDD
 // DDDD
 
-// const PROMPT = require("prompt-sync")();
+const PROMPT = require("prompt-sync")();
 
-// var altura = parseInt(PROMPT("Digite o número da altura da pirâmide: "));
+var altura = parseInt(PROMPT("Digite o número da altura da pirâmide: "));
 
-// var texto = "";
-// for (let i = 1; i <= altura; i++) {
-//     for (let j = 0; j < i; j++) {
-//         texto += "D";   
-//     }
-//     console.log(texto);             
-//     texto = "";
-// }
-
-// console.log("---");
 var texto = "";
-var altura = 4;
-
-//Linhas
 for (let i = 1; i <= altura; i++) {
-    
-    // D
     for (let j = 0; j < i; j++) {
-
-        // Space
-        for (let x = altura-i; x >= 0; x--) {
-            texto += " "; 
-        }
-
-        for (let y = 0; y < i; y++) {
-            texto += "D ";  
-        }
+        texto += "D";   
     }
     console.log(texto);             
     texto = "";
 }
+
+// // console.log("---");
+// var texto = "";
+// var altura = 4;
+
+// //Linhas
+// for (let i = 1; i <= altura; i++) {
+    
+//     // D
+//     for (let j = 0; j < i; j++) {
+
+//         // Space
+//         for (let x = altura-i; x >= 0; x--) {
+//             texto += " "; 
+//         }
+
+//         for (let y = 0; y < i; y++) {
+//             texto += "D ";  
+//         }
+//     }
+//     console.log(texto);             
+//     texto = "";
+// }
